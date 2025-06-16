5b602b52ca55641381e2bd1df2826e991c811ba8
PegasusGTV <tvgopalakrishnan5@gmail.com>
2025-06-16 18:05:39 -0400

    environment_final

--- environment.py
+++ environment.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-def build_environment(gridsize = 1000): 
+def build_environment(gridsize = 999): 
     world = np.zeros((gridsize, gridsize)) 
     start_x, start_y = 2, gridsize//2 
     goal_x, goal_y = gridsize - 3, gridsize//2 
     intermdiate_goal_zone = [200, 400] 
     constraint_zone = [401, 800] 
-    tunnel_width = 4 
-    d = 8 
-    num_tunnels = gridsize//8 
+    tunnel_width = 5 
+    d = 9 
+    num_tunnels = gridsize//9 
     world[constraint_zone[0]:constraint_zone[1], :] = 1
     return world, start_x, start_y, goal_x, goal_y, intermdiate_goal_zone, constraint_zone, tunnel_width, d, num_tunnels
