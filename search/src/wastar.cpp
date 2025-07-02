1f212dba09f05165eaa8483617aef06f3cea4e61
PegasusGTV <tvgopalakrishnan5@gmail.com>
2025-07-02 19:16:08 -0400

    wastar

--- search/src/wastar.cpp
+++ search/src/wastar.cpp
@@ -101,18 +101,18 @@ std::vector<Point> WaStar(const std::vector<std::vector<int>>& grid, const Point
 
 
 
-int main() {
-    std::vector<std::vector<int>> grid = {
-        {0,0,0,0},
-        {1,1,0,1},
-        {0,0,0,0},
-        {0,1,1,0}
-    };
-    Point start = {0, 0}, goal = {3, 3};
-    double weight = 2.0;
+// int main() {
+//     std::vector<std::vector<int>> grid = {
+//         {0,0,0,0},
+//         {1,1,0,1},
+//         {0,0,0,0},
+//         {0,1,1,0}
+//     };
+//     Point start = {0, 0}, goal = {3, 3};
+//     double weight = 2.0;
 
-    auto path = WaStar(grid, start, goal, weight);
-    for (const auto& p : path)
-        std::cout << "(" << p.x << ", " << p.y << ") ";
-    std::cout << "\n";
-}
+//     auto path = WaStar(grid, start, goal, weight);
+//     for (const auto& p : path)
+//         std::cout << "(" << p.x << ", " << p.y << ") ";
+//     std::cout << "\n";
+// }
