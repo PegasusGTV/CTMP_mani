e7a611d7836f563561fd932e5e1d2e5f66a647d5
Gopalakrishnan Thirunellai Venkitachalam <tvgopalakrishnan5@gmail.com>
2025-07-12 15:16:10 -0400

    downloaded map

--- src/create_try_map.cpp
+++ src/create_try_map.cpp
@@ -1,1 +1,13 @@
 #include "env_map.h"
+#include <iostream>
+#include <filesystem>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+int main(){
+    const std::string map_txt  = "../maps/map.txt";
+    const std::string map_meta = "../maps/map_meta.json";
+
+    Map Online_map = loadMapFromFiles_preprocessing(map_txt, map_meta);
+    
+}
