15de53cdeaa8abaeab14876ef1aaa71a09c5919d
Gopalakrishnan Thirunellai Venkitachalam <tvgopalakrishnan5@gmail.com>
2025-07-12 15:34:13 -0400

    progress

--- src/create_try_map.cpp
+++ src/create_try_map.cpp
@@ -9,5 +9,11 @@ int main(){
     const std::string map_meta = "../maps/map_meta.json";
 
     Map Online_map = loadMapFromFiles_preprocessing(map_txt, map_meta);
+
+    // int x_length = Online_map.x_length;
+
+    int number_of_tunnels = Online_map.x_length - Online_map.tunnel_width + 1;
+    // std::cout << "Number of tunnels: " << number_of_tunnels << "\n";
+    int num_of_groups = number_of_tunnels/Online_map.tunnel_width;
     
 }
