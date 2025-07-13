ca632926630819d23529d70e24d60dd4957a57d8
Gopalakrishnan Thirunellai Venkitachalam <tvgopalakrishnan5@gmail.com>
2025-07-12 20:33:37 -0400

    nearly formed map

--- src/create_try_map.cpp
+++ src/create_try_map.cpp
@@ -23,4 +23,27 @@ int main(){
 
     int choice = dist(rng);
 
+    // int tunnel_start, tunnel_end = choice, choice + Online_map.tunnel_width - 1; wrong
+    int tunnel_start = choice;
+    int tunnel_end   = choice + Online_map.tunnel_width - 1;
+
+    // int tunnel_start = choice,
+    // tunnel_end   = choice + Online_map.tunnel_width - 1; correct
+
+    int constraint_start = Online_map.constraint_zone[0];
+    int constraint_end = Online_map.constraint_zone[1];
+
+    int intermediate_goal_start = Online_map.intermediate_goal_zone[0];
+    int intermediate_goal_end = Online_map.intermediate_goal_zone[1];
+
+    for (int x = constraint_start; x <= constraint_end; ++x) {
+        for (int y = tunnel_start; y <= tunnel_end; ++y) {
+            Online_map.occupancy_grid[y][x] = 0; // Mark as free
+        }
+    }
+    
+
+
+
+
 }
