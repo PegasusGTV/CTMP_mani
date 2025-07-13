0b14f9fc433da54d7025734d9645793f7da7f60b
Gopalakrishnan Thirunellai Venkitachalam <tvgopalakrishnan5@gmail.com>
2025-07-13 15:02:58 -0400

    map loaded

--- src/online_ctmp.cpp
+++ src/online_ctmp.cpp
@@ -2,6 +2,8 @@
 #include "env_map.h"
 #include "reach_constraint.h"
 #include "tunnel_preprocessing.h"
+#include "nlohmann/json.hpp"
+using json = nlohmann::json;
 
 int main(){
     const std::string map_txt  = "../maps/map.txt";
@@ -11,7 +13,22 @@ int main(){
     const std::string rc_file  = "../data/reach_constraint.dat";
 
     Map env = loadMapFromFiles_preprocessing(map_txt, map_meta);
-    
+
+    std::ifstream jsonFile(out_json);
+    if(!jsonFile.is_open()) throw std::runtime_error("Could not open JSON file.");
+
+    json test_map;
+    jsonFile >> test_map;
+    env.occupancy_grid = test_map["map"]["occupancy_grid"].get<std::vector<std::vector<int>>>();
+
+    Point intermediate_goal_point(
+        test_map["map"]["intermediate_goal"][0],
+        test_map["map"]["intermediate_goal"][1]
+    );
+
+
+
+
 
 
 
