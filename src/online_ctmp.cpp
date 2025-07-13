a3f1ffcfe324c2ad71cf28736247994a11be2888
Gopalakrishnan Thirunellai Venkitachalam <tvgopalakrishnan5@gmail.com>
2025-07-13 14:31:49 -0400

    push

--- src/online_ctmp.cpp
+++ src/online_ctmp.cpp
@@ -7,6 +7,11 @@ int main(){
     const std::string map_txt  = "../maps/map.txt";
     const std::string map_meta = "../maps/map_meta.json";
     const std::string out_json  = "../maps/online_data.json";
+    const std::string out_bin  = "../data/test_tunnels.dat";
+    const std::string rc_file  = "../data/reach_constraint.dat";
+
+    Map env = loadMapFromFiles_preprocessing(map_txt, map_meta);
+    
 
 
 
