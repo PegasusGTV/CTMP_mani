7aff279518a1b49a353f711072a07c92bf28dd78
Gopalakrishnan Thirunellai Venkitachalam <tvgopalakrishnan5@gmail.com>
2025-07-13 21:34:00 -0400

    new changes

--- src/online_ctmp.cpp
+++ src/online_ctmp.cpp
@@ -26,6 +26,25 @@ int main(){
         test_map["map"]["intermediate_goal"][1]
     );
 
+    TunnelPreprocessor pre(env);
+    if (!pre.loadTunnelsFromFile(out_bin)) {
+      throw std::runtime_error("Failed to load tunnels from " + out_bin);
+    }
+
+    std::cout << "Reloaded " << pre.getTunnels().size() << " tunnels successfully.\n";
+
+    std::cout<< "number of tunnel groups: " << pre.num_tunnels_in_group_ << "\n";
+    std::cout<< "number of tunnels: " << pre.num_tunnels_ << "\n";
+    std::cout<< "number of tunnel groups: " << pre.num_tunnel_groups_ << "\n";
+
+
+    ReachConstraint rc(env);
+    if (!rc.loadFromFile(rc_file)) {
+      throw std::runtime_error("Failed to load ReachConstraint from " + rc_file);
+    }
+
+    std::cout << "number of tunnel groups: " << rc.tunnel_groups_.size() << "\n";
+    std::cout << "number of root paths to tunnel groups: " << rc.root_paths_to_tunnel_groups_.size() << "\n";
 
 
 
